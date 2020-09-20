e66b0d45e722d520d1ba517f047a6297d9fa2c34
Nathan Koike <nkoike1998@gmail.com>
2020-09-19 19:47:54 -1000

    fixed file so it runs "properly" all commands are echoed to the command prompt, which is annoying.

--- newsite.bat
+++ newsite.bat
@@ -1,37 +1,37 @@
-#!/bin/bash
-# the line above this lets us put this in our path and run it as a command
+REM this line lets us use a tab character
+set "TAB=  "
 
-# make a new directory for the site
+REM make a new directory for the site
 mkdir new_site
 
-# enter the directory
+REM enter the directory
 cd new_site
 
-# make the directories for css and js files
+REM make the directories for css and js files
 mkdir css
 mkdir scripts
 mkdir images
 
-# make the new index.html file and fill it with the beginnigns of a website
-echo "<!DOCTYPE html>
-<html lang=\"en-US\"> <!-- this page is in English -->
-  <head>
-    <meta charset=\"UTF-8\" /> <!-- use UTF-8 encoding -->
-    <title></title> <!-- put the title of your site here -->
-    <link rel=\"stylesheet\" type=text/css href=\"css/styles.css\" />
-    <!-- <link rel=\"icon\" href=\"images/icon.png\" /> -->
-  </head>
-  <body></body> <!-- put the body of your site here -->
-</html>" > index.html
+REM make the new index.html file and fill it with the beginnigns of a website
+echo ^<!DOCTYPE html^> >> index.html
+echo ^<html lang="en-US"^> ^<!-- this page is in English --^> >> index.html
+echo %TAB%^<head^> >> index.html
+echo %TAB%%TAB%^<meta charset="UTF-8"^> ^<!-- use UTF-8 encoding --^> >> index.html
+echo %TAB%%TAB%^<title^>^</title^> ^<!-- put the title of your site here --^> >> index.html
+echo %TAB%%TAB%^<link rel="stylesheet" type=text/css href="css/styles.css" ^> >> index.html
+echo %TAB%%TAB%^<!-- ^<link rel="icon" href="images/icon.png" ^> --^> >> index.html
+echo %TAB%^</head^> >> index.html
+echo %TAB%^<body^>^</body^> ^<!-- put the body of your site here --^> >> index.html
+echo ^</html^> >> index.html
 
-# go into the css folder
+REM go into the css folder
 cd css
 
-# make a blank template for css
+REM make a blank template for css
 echo > styles.css
 
-# go into the scrips directory
+REM go into the scrips directory
 cd ../scripts
 
-# make a new script template file
+REM make a new script template file
 echo > script.js
